dd04d74628166025629792c78d6bca5fd377ee9f
Fumz <funmi887@gmail.com>
2020-03-29 11:25:52 +0100

    push my first code

---
+++ src/test/java/Login/LoginTests.java
@@ -0,0 +1,32 @@
+package Login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginTests {
+
+    public void setUp() throws InterruptedException
+    { System.setProperty("webdriver.chrome.driver","resources/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.facebook.com/");
+
+        Thread.sleep(5000);
+        driver.manage().window().maximize();
+
+        System.out.println(driver.getTitle());
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.findElement(By.id("email")).sendKeys("funmi887@gmail.com");
+
+        driver.findElement(By.id("pass")).sendKeys("fop4com8");
+        driver.findElement(By.id("loginbutton")).click(); }
+
+    public static void main(String args[]) throws InterruptedException {
+        LoginTests test = new LoginTests();
+        test.setUp();
+    }
+}
